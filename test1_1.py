447ef16b804036fcce89b2aa88a6950bbff57858
Wenjian Lv <lw2906918681@gmail.com>
2024-12-16 18:08:10 +0800

    12/16

--- test1_1.py
+++ test1_1.py
@@ -2,7 +2,7 @@ import diplib as dip
 import math
 
 # Load image and set pixel size
-img = dip.ImageRead('N6uqw.jpg')
+# img = dip.ImageRead('N6uqw.jpg')
 # dip.Show(img)
 # dip.viewer.Show(img)
 # dip.viewer.ShowModal(img)
@@ -13,4 +13,35 @@ img = dip.ImageRead('N6uqw.jpg')
 # dip.viewer.Spin()
 # dip.viewer.Draw()
 
-img.Show('normal')
+# img.Show('normal')
+
+
+#!/usr/bin/env python3
+
+# Tests some functions for finding circles using the Hough transform
+
+# Prepare image
+a = dip.Image((512, 512))
+a.Fill(0)
+dip.DrawEllipsoid(a, (200, 200), (256, 256))
+dip.DrawEllipsoid(a, (50, 50), (350, 350))
+
+gv = dip.Gradient(a)
+gm = dip.Norm(gv)
+bin = dip.IsodataThreshold(gm)
+
+# Find circles using low-level functions
+h = dip.HoughTransformCircleCenters(bin, gv)
+m = dip.FindHoughMaxima(h, 10)
+d = dip.PointDistanceDistribution(bin, m)
+r = d.MaximumLikelihood()
+print(m, r)
+print('--------------------')
+# Find circles using integrated function
+c = dip.FindHoughCircles(bin, gv, (), 10)
+print(c)
+
+# Show original and transform
+dip.viewer.Show(bin, "Input")
+dip.viewer.Show(h, "Hough transform")
+dip.viewer.Spin()
