7af4c8cc2b5bddcdad42e22917eba50760c0d77b
Wenjian Lv <lw2906918681@gmail.com>
2024-12-20 17:41:12 +0800

    12/20

--- 11111.py
+++ 11111.py
@@ -1,10 +1,4 @@
-# 计算图像的亚像素点并绘制（matplotlib以及cv2），并对亚像素点进行3种椭圆方式拟合绘制
-# python zernick_detection.py
-
-import time
-
 import cv2
-import imutils
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -66,51 +60,35 @@ M40 = np.array([0, 0.013, 0.0056, -0.0018, 0.0056, 0.013, 0,
                 0.0130, -0.0186, -0.0323, -0.0239, -0.0323, -0.0186, 0.0130,
                 0, 0.013, 0.0056, -0.0018, 0.0056, 0.013, 0]).reshape(7, 7)
 
-
-def zernike_detection(path):
-    time1 = time.time()
-    img = cv2.imread(path)
-    print('size: ', img.shape)
-    # imutils.resize(img, height=600)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blur_img = cv2.medianBlur(img, 13)
-    c_img = cv2.adaptiveThreshold(blur_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 7, 4)
-    time2 = time.time()
-    print('读取加上预处理时间: ', time2 - time1)
-    cv2.namedWindow('c_img', 0)
-    cv2.imshow('c_img', c_img)
-    time3 = time.time()
-    ZerImgM00 = cv2.filter2D(c_img, cv2.CV_64F, M00)
-    ZerImgM11R = cv2.filter2D(c_img, cv2.CV_64F, M11R)
-    ZerImgM11I = cv2.filter2D(c_img, cv2.CV_64F, M11I)
-    ZerImgM20 = cv2.filter2D(c_img, cv2.CV_64F, M20)
-    ZerImgM31R = cv2.filter2D(c_img, cv2.CV_64F, M31R)
-    ZerImgM31I = cv2.filter2D(c_img, cv2.CV_64F, M31I)
-    ZerImgM40 = cv2.filter2D(c_img, cv2.CV_64F, M40)
+def zernike_detection(image):
+    blur_img = cv2.medianBlur(image, 3)
+    ret, image = cv2.threshold(blur_img, 127, 255, cv2.THRESH_BINARY_INV)
+    cv2.imshow('image', image)
+    ZerImgM00 = cv2.filter2D(image, cv2.CV_64F, M00)
+    ZerImgM11R = cv2.filter2D(image, cv2.CV_64F, M11R)
+    ZerImgM11I = cv2.filter2D(image, cv2.CV_64F, M11I)
+    ZerImgM20 = cv2.filter2D(image, cv2.CV_64F, M20)
+    ZerImgM31R = cv2.filter2D(image, cv2.CV_64F, M31R)
+    ZerImgM31I = cv2.filter2D(image, cv2.CV_64F, M31I)
+    ZerImgM40 = cv2.filter2D(image, cv2.CV_64F, M40)
 
     point_temporary_x = []
     point_temporary_y = []
     scatter_arr = cv2.findNonZero(ZerImgM00).reshape(-1, 2)
     for idx in scatter_arr:
         j, i = idx
-        theta_temporary = np.arctan2(ZerImgM31I[i][j], ZerImgM31R[i][j])
+        theta_temporary = np.arctan2(ZerImgM31I[i][j], ZerImgM31R[i][j])    
         rotated_z11 = np.sin(theta_temporary) * ZerImgM11I[i][j] + np.cos(theta_temporary) * ZerImgM11R[i][j]
         rotated_z31 = np.sin(theta_temporary) * ZerImgM31I[i][j] + np.cos(theta_temporary) * ZerImgM31R[i][j]
         l_method1 = np.sqrt((5 * ZerImgM40[i][j] + 3 * ZerImgM20[i][j]) / (8 * ZerImgM20[i][j]))
-
         l_method2 = np.sqrt((5 * rotated_z31 + rotated_z11) / (6 * rotated_z11))
-
         l = (l_method1 + l_method2) / 2
-
         k = 3 * rotated_z11 / (2 * (1 - l_method2 ** 2) ** 1.5)
 
-        # h = (ZerImgM00[i][j] - k * np.pi / 2 + k * np.arcsin(l_method2) + k * l_method2 * (1 - l_method2 ** 2) ** 0.5)
-        # / np.pi
-        k_value = 20.0
-        l_value = 2 ** 0.5 / g_N
+        k_value = 200
+        l_value = 2 ** 0.5 / g_N / 2
 
         absl = np.abs(l_method2 - l_method1)
-
         if k >= k_value and absl <= l_value:
             y = i + g_N * l * np.sin(theta_temporary) / 2
             x = j + g_N * l * np.cos(theta_temporary) / 2
@@ -118,72 +96,14 @@ def zernike_detection(path):
             point_temporary_y.append(y)
         else:
             continue
-    time4 = time.time()
-    print('检测时间: ', time4 - time3)
-    return point_temporary_x, point_temporary_y, c_img
+    
+    return point_temporary_x, point_temporary_y, image
 
 
-image = cv2.imread('D:\\chrome_downloads\\ym.jpg')
-path = 'ym_600.jpg'
-cv2.imwrite(path, imutils.resize(image, height=600))
-# path = 'D:\\chrome_downloads\\ym.jpg'
-time1 = time.time()
-point_temporary_x, point_temporary_y, c_img = zernike_detection(path)
-time2 = time.time()
-print('time: ', time2 - time1)  # 运算速度太慢了，可以考虑使用多线程进行加速
-
-# gray : 进行检测的图像
-gray = cv2.imread(path, 0)
-plt.rcParams['figure.dpi'] = 72
-# plt.imshow(gray, cmap="gray")
-plt.imshow(c_img, cmap="gray")
-# point检测出的亚像素点
-point = np.array([point_temporary_x, point_temporary_y])
-# s:调整显示点的大小
-plt.scatter(point[0, :], point[1, :], s=10, marker="*") #颜色为蓝色
-plt.show()
-
-
-def cv_fit_ellipse(points, flag=0):
-    # cv2.fitEllipse only can estimate int numpy 2D array data
-    p = np.array(points)
-    p = np.array(p).astype(int)
-    center0, axes0, angle0 = cv2.fitEllipse(p)
-    center1, axes1, angle1 = cv2.fitEllipseAMS(p)
-    center2, axes2, angle2 = cv2.fitEllipseDirect(p)
-    print(type(center0))
-    print("fitEllipse:       " + str(np.array(center0)))
-    print("fitEllipseAMS:    " + str(np.array(center1)))
-    print("fitEllipseDirect: " + str(np.array(center2)))
-    box = tuple([tuple(np.array(center0)), tuple(np.array(axes0)), angle0])
-    if flag == 0:
-        box = tuple([tuple(np.array(center0)), tuple(np.array(axes0)), angle0])
-    elif flag == 1:
-        box = tuple([tuple(np.array(center1)), tuple(np.array(axes1)), angle1])
-    else:
-        box = tuple([tuple(np.array(center0)), tuple(np.array(axes0)), angle0])
-    return box
-
-
-points = np.array(list(zip(point_temporary_x, point_temporary_y)))
-print(type(points), points[0], points.shape)
-box1 = cv_fit_ellipse(points, flag=1)
-
-image = cv2.imread(path)
-
-cv2.ellipse(img=image, box=box1, color=(255, 0, 255), thickness=1)
-cv2.namedWindow('image', 0)
-cv2.imshow("image", image)
-for i in points:
-    # 亚像素点绘制随机颜色半径圆
-    radius = np.random.randint(1, high=10)
-    color = np.random.randint(0, high=256, size=(3,)).tolist()
-    # print('radius: ', radius, type(radius))
-    # print('color: ', color, type(color))
-    cv2.circle(image, (int(i[0]), int(i[1])), radius, color, -1)
-
-cv2.namedWindow("image1", 0)
-cv2.imshow("image1", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
+if __name__ == '__main__':
+    img_circle = cv2.imread('img_circle.png', cv2.IMREAD_GRAYSCALE)
+    point_temporary_x, point_temporary_y, image = zernike_detection(img_circle)
+    plt.imshow(image, cmap='gray')
+    point = np.array([point_temporary_x, point_temporary_y])
+    plt.scatter(point[0, :], point[1, :], s = 1, marker="*")
+    plt.show()
